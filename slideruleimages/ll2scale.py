619861b424425a6c45575fb9d3bc2ec14da11ab1
Bernard Czenkusz <bernie@skipole.co.uk>
2026-04-01 11:37:21 +0100

    LL2 orientation done

--- slideruleimages/ll2scale.py
+++ slideruleimages/ll2scale.py
@@ -2,49 +2,38 @@ import xml.etree.ElementTree as ET
 
 import math
 
-
-def _vertical(doc, length, xpos, ybot, col="black"):
-    """Creates a vertical line
-       doc is the scale element to which this line will be inserted
-       length is the length of the vertical line
-       ybot is the ending y position
-       xpos is the x position
-       col is the colour of the line"""
-    # get xpos to the nearest .25
-    xpos = round(xpos*4)/4.0
-    vline = {"x1":str(xpos), "y1":str(ybot), "x2":str(xpos), "y2":str(ybot-length), "style":f"stroke:{col};stroke-width:1"}
-    ET.SubElement(doc, 'line', vline)
-
-def _text(doc, textstr, xpos, texty, fontsize):
-    "Adds text"
-    if textstr:
-        if len(textstr) == 1:
-            textpos = round(xpos - 4)    #  textpos This is in pixels
-        else:
-            textpos = round(xpos - 6)    # three characters, such as 1.5
-        
-        tel = ET.SubElement(doc, 'text', {"x":str(textpos), "y":str(texty),"fill":"black", "font-size":str(fontsize)})
-        tel.text = textstr
+from . import draw
 
 
 def addLL2scale(rl, rightmove, zero) -> ET.Element:
     "Adds the LL2 scale to the rule, returns the scale element"
 
-    ybot = 100
-
     doc = ET.Element('g')
 
     # LL2 mark
-    LLmark = ET.SubElement(doc, 'text', {"x":str(rightmove + 6), "y":str(ybot-30), "fill":"black", "font-size":"24"})
-    LLmark.text = "LL"
-    LL2mark = ET.SubElement(doc, 'text', {"x":str(rightmove + 34), "y":str(ybot-28), "fill":"black", "font-size":"12"})
-    LL2mark.text = "2"
+    if zero:
+        LLmark = ET.SubElement(doc, 'text', {"x":str(rightmove + 6), "y":"40", "fill":"black", "font-size":"24"})
+        LLmark.text = "LL"
+        LL2mark = ET.SubElement(doc, 'text', {"x":str(rightmove + 34), "y":"42", "fill":"black", "font-size":"12"})
+        LL2mark.text = "2"
+    else:
+        LLmark = ET.SubElement(doc, 'text', {"x":str(rightmove + 6), "y":"70", "fill":"black", "font-size":"24"})
+        LLmark.text = "LL"
+        LL2mark = ET.SubElement(doc, 'text', {"x":str(rightmove + 34), "y":"72", "fill":"black", "font-size":"12"})
+        LL2mark.text = "2"
+
 
     # e**0.1x mark
-    emark = ET.SubElement(doc, 'text', {"x":str(rightmove  + rl.leftmargin + rl.scalewidth + 14), "y":str(ybot-36),"fill":"black", "font-size":"16"})
-    emark.text = "e"
-    exmark = ET.SubElement(doc, 'text', {"x":str(rightmove  + rl.leftmargin + rl.scalewidth + 22), "y":str(ybot-44),"fill":"black", "font-size":"12"})
-    exmark.text = "0.1x"
+    if zero:
+        emark = ET.SubElement(doc, 'text', {"x":str(rightmove  + rl.leftmargin + rl.scalewidth + 14), "y":"44","fill":"black", "font-size":"16"})
+        emark.text = "e"
+        exmark = ET.SubElement(doc, 'text', {"x":str(rightmove  + rl.leftmargin + rl.scalewidth + 22), "y":"36","fill":"black", "font-size":"12"})
+        exmark.text = "0.1x"
+    else:
+        emark = ET.SubElement(doc, 'text', {"x":str(rightmove  + rl.leftmargin + rl.scalewidth + 14), "y":"64","fill":"black", "font-size":"16"})
+        emark.text = "e"
+        exmark = ET.SubElement(doc, 'text', {"x":str(rightmove  + rl.leftmargin + rl.scalewidth + 22), "y":"56","fill":"black", "font-size":"12"})
+        exmark.text = "0.1x"
 
 
     # scaling with y = mx+c
@@ -54,16 +43,25 @@ def addLL2scale(rl, rightmove, zero) -> ET.Element:
     xpos = m*math.log10(math.log(1.11)) + c
     textstr = "1.11"
     fontsize = 14
-    texty = ybot-30
-    _text(doc, textstr, xpos, texty, fontsize)
+    y0 = 40
+    y1 = 30
+    draw.text(doc, textstr, xpos, zero, y0, y1, fontsize)
+
+    xpos = m*math.log10(math.log(1.13)) + c
+    textstr = "1.13"
+    fontsize = 14
+    y0 = 40
+    y1 = 30
+    draw.text(doc, textstr, xpos, zero, y0, y1, fontsize)
 
     xpos = c
     textstr = "e"
     fontsize = 14
-    texty = ybot-55
-    _text(doc, textstr, xpos, texty, fontsize)
+    y0 = 60
+    y1 = 50
+    draw.text(doc, textstr, xpos+2, zero, y0, y1, fontsize)
     length = 40
-    _vertical(doc, length, xpos, ybot, col="black")
+    draw.line(doc, length, xpos, zero, col="black")
 
 
     # x from 1.105 to 2.0 in steps of 0.001
@@ -76,13 +74,15 @@ def addLL2scale(rl, rightmove, zero) -> ET.Element:
             length = 45
             textstr = str(x)
             fontsize = 16
-            texty = ybot-50
+            y0 = 60
+            y1 = 50
         elif r % 50 == 0:
             length = 35
             if r < 1500:
                 textstr = str(x)
                 fontsize = 14
-                texty = ybot-40
+                y0 = 50
+                y1 = 40
         elif r % 10 == 0:
             length = 25
         elif r % 5 == 0 and r<1300:
@@ -90,9 +90,9 @@ def addLL2scale(rl, rightmove, zero) -> ET.Element:
         elif r < 1140:
             length = 15
         if length:
-            _vertical(doc, length, xpos, ybot, col="black")
+            draw.line(doc, length, xpos, zero, col="black")
         if textstr:
-            _text(doc, textstr, xpos, texty, fontsize)
+            draw.text(doc, textstr, xpos, zero, y0, y1, fontsize)
 
     # x from 2 to 2.7 in steps of 0.01
     for r in range(200, 271):
@@ -105,13 +105,14 @@ def addLL2scale(rl, rightmove, zero) -> ET.Element:
             if r < 240 or r==250:
                 textstr = str(x)
                 fontsize = 12
-                texty = ybot-50
+                y0 = 60
+                y1 = 50
         elif r % 5 == 0:
             length = 35
         if length:
-            _vertical(doc, length, xpos, ybot, col="black")
+            draw.line(doc, length, xpos, zero, col="black")
         if textstr:
-            _text(doc, textstr, xpos, texty, fontsize)
+            draw.text(doc, textstr, xpos, zero, y0, y1, fontsize)
 
     return doc
 
